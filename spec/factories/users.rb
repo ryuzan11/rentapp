df3b53ee5e34c00cdb8e94ffec8873cd1c8f998d
Yuta Osaka <yusoac@gmail.com>
2019-05-07 21:03:20 +0900

    user scaffoldを実行

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    username { "MyString" }
+    password { "MyString" }
+    email { "MyString" }
+    message { "MyString" }
+    gender_id { 1 }
+  end
+end
