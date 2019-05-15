bd730a7dca52491ce7456bedf91fc0bf4bc4d640
Yuta Osaka <yusoac@gmail.com>
2019-05-16 01:00:07 +0900

    ユーザ機能のroot定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  root 'users#index'
+
+  devise_for :users, :controllers => {
+   :registrations => 'users/registrations',
+   :sessions => 'users/sessions',
+   :passwords => 'users/passwords'
+  }
+
+  devise_scope :user do
+    get 'login' => 'users/sessions#new'
+    get 'sign_up' => 'users/registrations#new'
+    get 'edit' => 'users/registrations#edit'
+    delete 'logout' => 'users/sessions#destroy'
+  end
+
+  resources :users, :only => [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
