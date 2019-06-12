39d3a628b0f2060f51977b3d6e4dd67f88f7166c
Yuta Osaka <yusoac@gmail.com>
2019-06-12 20:44:39 +0900

    マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_062400) do
-
-  create_table "building_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "building_type"
-  end
+ActiveRecord::Schema.define(version: 2019_06_12_063509) do
 
   create_table "cities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "prefecture_id"
@@ -23,20 +19,16 @@ ActiveRecord::Schema.define(version: 2019_06_10_062400) do
     t.index ["prefecture_id"], name: "index_cities_on_prefecture_id"
   end
 
-  create_table "constructions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "construction"
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "rent_house_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "genders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "gender"
-  end
-
-  create_table "layouts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "layout"
-  end
-
-  create_table "olds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "old"
   end
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -45,36 +37,20 @@ ActiveRecord::Schema.define(version: 2019_06_10_062400) do
 
   create_table "rent_houses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "construction_id"
     t.bigint "prefecture_id", null: false
     t.bigint "city_id", null: false
-    t.bigint "walk_time_id"
-    t.bigint "layout_id"
-    t.bigint "building_type_id"
-    t.bigint "old_id"
     t.string "station_name", null: false
     t.string "line_name"
     t.integer "rent", null: false
     t.integer "management_fee"
     t.integer "else_fee"
-    t.string "separate"
-    t.integer "floor"
-    t.string "woman"
-    t.string "balconi"
-    t.string "daylight"
     t.text "comment"
-    t.string "room_size"
     t.string "building_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["building_type_id"], name: "index_rent_houses_on_building_type_id"
     t.index ["city_id"], name: "index_rent_houses_on_city_id"
-    t.index ["construction_id"], name: "index_rent_houses_on_construction_id"
-    t.index ["layout_id"], name: "index_rent_houses_on_layout_id"
-    t.index ["old_id"], name: "index_rent_houses_on_old_id"
     t.index ["prefecture_id"], name: "index_rent_houses_on_prefecture_id"
     t.index ["user_id"], name: "index_rent_houses_on_user_id"
-    t.index ["walk_time_id"], name: "index_rent_houses_on_walk_time_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -93,17 +69,8 @@ ActiveRecord::Schema.define(version: 2019_06_10_062400) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "walk_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "walk_time"
-  end
-
   add_foreign_key "cities", "prefectures"
-  add_foreign_key "rent_houses", "building_types"
   add_foreign_key "rent_houses", "cities"
-  add_foreign_key "rent_houses", "constructions"
-  add_foreign_key "rent_houses", "layouts"
-  add_foreign_key "rent_houses", "olds"
   add_foreign_key "rent_houses", "prefectures"
   add_foreign_key "rent_houses", "users"
-  add_foreign_key "rent_houses", "walk_times"
 end
