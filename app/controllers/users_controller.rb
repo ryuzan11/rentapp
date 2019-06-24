fa9b7e99e39789f912c0def3b71bf3e54ede4be2
Yuta Osaka <yusoac@gmail.com>
2019-06-25 00:36:02 +0900

    Create like controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @gender = Gender.find(@user.gender_id)
     @renthouses = RentHouse.where(user_id: @user.id).order("id DESC")
+    @like_rentHouses = @user.like_rentHouses
   end
 end
