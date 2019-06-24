d950ebdbb750857279373e9a8ee8d00297ceb09e
Yuta Osaka <yusoac@gmail.com>
2019-06-25 00:36:43 +0900

    Define like model

--- app/models/rent_house.rb
+++ app/models/rent_house.rb
@@ -8,4 +8,20 @@ class RentHouse < ApplicationRecord
   # belongs_to :layout, optional: true
   # belongs_to :buildingType, optional: true
   # belongs_to :old, optional: true
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
+
+  def fav(user)
+    likes.create!(user_id: user.id)
+  end
+
+  def unfav(user)
+    likes.find_by(user_id: user.id).destroy
+  end
+
+  def fav?(user)
+    # like_users.where(user_id: user.id).exists?
+    like_users.include?(user)
+  end
+
 end
