9ae697a5f7fee7a4834621f7de788d1532c97e03
Yuta Osaka <yusoac@gmail.com>
2019-07-25 05:34:43 +0900

    コメント非同期通信実装

--- app/assets/javascripts/renthouse_comment.js
+++ app/assets/javascripts/renthouse_comment.js
@@ -2,36 +2,42 @@ $(function(){
   function buildHTML(comment){
     var html = `<p>
                   <strong>
-                    <a href=/users/${comment.user_id}>${comment.user_nickname}</a>
-                    :
-                    </strong>
-                    ${comment.text}
+                  <a href=/users/${comment.user_id}>${comment.user_nickname}</a>：
+                  </strong>
+                  ${comment.text}
                 </p>`
     return html;
   }
-  $('#renthouse_comment').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      console.log(html)
-      $('.comments').append(html)
-      $('.textbox').val('')
-    })
-    .fail(function(){
-      alart('エラー');
-    })
-    // .always(function(){
-    //   $('.form__submit').prop('disabled',false);
-    // })
+
+  $(document).on('ajax:success','#renthouse_comment',(e) => {
+    let data = e.originalEvent.detail[0]
+    let html = buildHTML(data);
+    $('.comments').append(html)
+    $('.textbox').val('')
   })
 })
+//   $('#renthouse_comment').on('submit', {passive: false}, function(e) {
+//     console.log(e)
+//     e.preventDefault();
+//     console.log(e)
+//     var formData = new FormData(this);
+//     // console.log(formData)
+//     var url = $(this).attr('action')
+//     $.ajax({
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(data){
+//       var html = buildHTML(data);
+//       $('.comments').append(html)
+//       $('.textbox').val('')
+//     })
+//     .fail(function(){
+//       alart('エラー');
+//     })
+//   })
+// })
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    # binding.pry()
-    @comment = Comment.create(text: comment_params[:text], rent_house_id: comment_params[:rent_house_id], user_id: current_user.id)
-    respond_to do |format|
-      format.html { redirect_to rent_house_path(id: comment_params[:rent_house_id]) }
-      format.json
+    @comment = Comment.new(text: comment_params[:text], rent_house_id: comment_params[:rent_house_id], user_id: current_user.id)
+    if @comment.save
+      respond_to do |format|
+        # format.html { redirect_to rent_house_path(id: comment_params[:rent_house_id]) }
+        format.json
+      end
+    else
+      render json: @comment.error, status: :unprocessable_entity
     end
+    # binding.pry()
   end
 
   private
