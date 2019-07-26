acb74cd2d2542ab2aac9250f3540fd1e123a89f7
Yuta Osaka <yusoac@gmail.com>
2019-07-26 16:15:10 +0900

    ユーザー詳細タブ機能を実装
    非同期でタブ移動

--- app/assets/javascripts/mypage_tab.js
+++ app/assets/javascripts/mypage_tab.js
@@ -1,3 +1,8 @@
-// $(funciton(){
-//   $('div.tab-pane').hide();
-// });
+$(document).on('click','.nav-item', function(){
+    $('.show').removeClass('show');
+    $(this).addClass('show');
+    $('.active').removeClass('active');
+    const index = $(this).index();
+    $('.tab-pane').eq(index).addClass('active');
+  }
+)
