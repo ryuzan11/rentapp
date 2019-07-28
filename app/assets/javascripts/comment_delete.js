70ff5db2d8c48103c75b651d8bd1545bfa0a65d8
Yuta Osaka <yusoac@gmail.com>
2019-07-29 02:48:42 +0900

    Comment削除機能を調整

---
+++ app/assets/javascripts/comment_delete.js
@@ -0,0 +1,13 @@
+$(function(){
+  $(document).on('click', '[data-open="modal-comment"]', function(e) {
+    // console.log('open')
+    var modalId = $(e.target).attr('data-modal')
+    // console.log(modalId)
+    $('#' + modalId).addClass('is-show').addClass('is-animate')
+  })
+  $(document).on('click', '[data-close="modal-comment"]', function(e) {
+    // console.log('close')
+    var modalId = $(e.target).attr('data-modal')
+    $('#' + modalId).removeClass('is-animate').removeClass('is-show')
+  })
+});
--- app/assets/javascripts/renthouse_comment.js
+++ app/assets/javascripts/renthouse_comment.js
@@ -1,25 +1,25 @@
 $(function(){
   function buildHTML(comment,user_nickname){
-    var html = `<p>
+    var html = `<div>
                   <strong>
                   <a href=/users/${comment.user_id}>${user_nickname}</a>：
                   </strong>
                   ${comment.text}
-                </p>
-                <button data-modal="delete-item" data-open="modal" class="btn btn-outline-danger">削除</button>
-                <aside id="delete-item" class="modal">
-                  <div class="modal-inner modal-banner">
-                    <div class="modal-body">
-                      <div class="modal-head bold">確認<br>
-                        削除すると二度と復活できません。<br><br>本当に削除しますか？
+                  <button data-modal="delete-comment" data-open="modal-comment" class="far fa-trash-alt""></button>
+                  <aside id="delete-comment" class="modal-comment">
+                    <div class="modal-inner modal-banner">
+                      <div class="modal-body">
+                        <div class="modal-head bold">確認<br>
+                          削除すると二度と復活できません。<br><br>本当に削除しますか？
+                        </div>
+                        <p>
+                        <button type="button" class="modal-btn modal-btn-cancel" data-modal="delete-comment" data-close="modal-comment">キャンセル</button>
+                        <a class="modal-btn modal-btn-submit" rel="nofollow" data-method="delete" href="/rent_houses/${comment.rent_house_id}/comments/${comment.id}">削除する</a>
+                        </p>
                       </div>
-                      <p>
-                      <button type="button" class="modal-btn modal-btn-cancel" data-modal="delete-item" data-close="modal">キャンセル</button>
-                      <a class="modal-btn modal-btn-submit" rel="nofollow" data-method="delete" href="/rent_houses/${comment.rent_house_id}/comments/${comment.id}">削除する</a>
-                      </p>
                     </div>
-                  </div>
-                </aside>`
+                  </aside>
+                </div>`
     return html;
   }
 
