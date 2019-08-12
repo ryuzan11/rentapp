b813fe7cef2299cf183e081e9697e71f7de136b0
Yuta Osaka <yusoac@gmail.com>
2019-08-12 16:19:09 +0900

    デザイン修正

--- app/assets/javascripts/mypage_tab.js
+++ app/assets/javascripts/mypage_tab.js
@@ -1,8 +1,8 @@
 $(document).on('click','.nav-item', function(){
-    $('.show').removeClass('show');
-    $(this).addClass('show');
-    $('.active').removeClass('active');
-    const index = $(this).index();
-    $('.tab-pane').eq(index).addClass('active');
+  $('.show').removeClass('show');
+  $(this).addClass('show');
+  $('.active').removeClass('active');
+  const index = $(this).index();
+  $('.tab-pane').eq(index).addClass('active');
   }
 )
